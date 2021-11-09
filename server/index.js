6cd053976987d0d9f590df5671072aa6845633df
4501104220 <thangtrantt@hotmail.com>
2021-11-09 16:38:54 +0700

    fix: server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -7,18 +7,21 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+app.get('/',(req, res)=>{
+  res.send('Hello API');
+})
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
